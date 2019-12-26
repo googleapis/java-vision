d6a42f30dd59c00ff3889fbb2b4aef165d722267
Praful Makani <praful@qlogic.io>
2019-12-26 15:23:09 +0530

    test: add productset and referenceimage

--- google-cloud-vision/src/test/java/com/google/cloud/vision/it/ITSystemTest.java
+++ google-cloud-vision/src/test/java/com/google/cloud/vision/it/ITSystemTest.java
@@ -25,6 +25,7 @@ import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.vision.v1.AddProductToProductSetRequest;
 import com.google.cloud.vision.v1.AnnotateFileResponse;
 import com.google.cloud.vision.v1.AnnotateImageRequest;
 import com.google.cloud.vision.v1.AnnotateImageResponse;
@@ -34,9 +35,13 @@ import com.google.cloud.vision.v1.AsyncBatchAnnotateFilesResponse;
 import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
 import com.google.cloud.vision.v1.ColorInfo;
 import com.google.cloud.vision.v1.CreateProductRequest;
+import com.google.cloud.vision.v1.CreateProductSetRequest;
+import com.google.cloud.vision.v1.CreateReferenceImageRequest;
 import com.google.cloud.vision.v1.CropHint;
 import com.google.cloud.vision.v1.CropHintsAnnotation;
 import com.google.cloud.vision.v1.DeleteProductRequest;
+import com.google.cloud.vision.v1.DeleteProductSetRequest;
+import com.google.cloud.vision.v1.DeleteReferenceImageRequest;
 import com.google.cloud.vision.v1.DominantColorsAnnotation;
 import com.google.cloud.vision.v1.EntityAnnotation;
 import com.google.cloud.vision.v1.FaceAnnotation;
@@ -44,13 +49,18 @@ import com.google.cloud.vision.v1.Feature;
 import com.google.cloud.vision.v1.GcsDestination;
 import com.google.cloud.vision.v1.GcsSource;
 import com.google.cloud.vision.v1.GetProductRequest;
+import com.google.cloud.vision.v1.GetProductSetRequest;
+import com.google.cloud.vision.v1.GetReferenceImageRequest;
 import com.google.cloud.vision.v1.Image;
 import com.google.cloud.vision.v1.ImageAnnotatorClient;
 import com.google.cloud.vision.v1.ImageContext;
 import com.google.cloud.vision.v1.ImageSource;
 import com.google.cloud.vision.v1.InputConfig;
 import com.google.cloud.vision.v1.Likelihood;
+import com.google.cloud.vision.v1.ListProductSetsRequest;
+import com.google.cloud.vision.v1.ListProductsInProductSetRequest;
 import com.google.cloud.vision.v1.ListProductsRequest;
+import com.google.cloud.vision.v1.ListReferenceImagesRequest;
 import com.google.cloud.vision.v1.LocalizedObjectAnnotation;
 import com.google.cloud.vision.v1.LocationName;
 import com.google.cloud.vision.v1.OperationMetadata;
@@ -58,8 +68,14 @@ import com.google.cloud.vision.v1.OutputConfig;
 import com.google.cloud.vision.v1.Product;
 import com.google.cloud.vision.v1.ProductName;
 import com.google.cloud.vision.v1.ProductSearchClient;
+import com.google.cloud.vision.v1.ProductSet;
+import com.google.cloud.vision.v1.ProductSetName;
+import com.google.cloud.vision.v1.ReferenceImage;
+import com.google.cloud.vision.v1.ReferenceImageName;
+import com.google.cloud.vision.v1.RemoveProductFromProductSetRequest;
 import com.google.cloud.vision.v1.SafeSearchAnnotation;
 import com.google.cloud.vision.v1.TextAnnotation;
+import com.google.cloud.vision.v1.UpdateProductRequest;
 import com.google.cloud.vision.v1.Vertex;
 import com.google.cloud.vision.v1.WebDetection;
 import com.google.cloud.vision.v1.WebDetectionParams;
@@ -86,6 +102,12 @@ public class ITSystemTest {
   private static Product product;
   private static String formatProductName;
   private static String productName;
+  private static ProductSet productSet;
+  private static String productSetName;
+  private static String formatProductSetName;
+  private static ReferenceImage referenceImage;
+  private static String referenceImageName;
+  private static String formatReferenceImageName;
   private static final String PROJECT_ID = ServiceOptions.getDefaultProjectId();
   private static final String ID = UUID.randomUUID().toString().substring(0, 8);
   private static final String OUTPUT_PREFIX = "OCR_PDF_TEST_OUTPUT_" + ID;
@@ -96,19 +118,23 @@ public class ITSystemTest {
   private static final String COMPUTE_REGION = "us-west1";
   private static final String LOCATION_NAME =
       LocationName.of(PROJECT_ID, COMPUTE_REGION).toString();
-  private static final String PRODUCT_DISPLAY_NAME = "test-display-name-" + ID;
+  private static final String PRODUCT_DISPLAY_NAME = "test-product-display-name-" + ID;
   private static final String PRODUCT_CATEGORY = "homegoods";
   private static final String PRODUCT_ID = "test-product-" + ID;
   private static final String KEY = "test-key-" + ID;
   private static final String VALUE = "test-value-" + ID;
+  private static final Product.KeyValue KEY_VALUE =
+      Product.KeyValue.newBuilder().setKey(KEY).setValue(VALUE).build();
+  private static final String PRODUCT_SET_ID = "test-product-set-" + ID;
+  private static final String PRODUCT_SET_DISPLAY_NAME = "test-product-set-display-name-" + ID;
 
   @BeforeClass
   public static void setUp() throws IOException {
 
     imageAnnotatorClient = ImageAnnotatorClient.create();
 
+    /* create product */
     productSearchClient = ProductSearchClient.create();
-    // create product
     Product createProduct =
         Product.newBuilder()
             .setName(PRODUCT_ID)
@@ -121,17 +147,70 @@ public class ITSystemTest {
             .setProduct(createProduct)
             .build();
     product = productSearchClient.createProduct(productRequest);
-    productName = product.getName().substring(product.getName().lastIndexOf("/")).replace("/", "");
+    productName = getName(product.getName());
     formatProductName = ProductName.of(PROJECT_ID, COMPUTE_REGION, productName).toString();
+
+    /* create product set */
+    ProductSet createProductSet =
+        ProductSet.newBuilder().setDisplayName(PRODUCT_SET_DISPLAY_NAME).build();
+    CreateProductSetRequest productSetRequest =
+        CreateProductSetRequest.newBuilder()
+            .setParent(LOCATION_NAME)
+            .setProductSet(createProductSet)
+            .setProductSetId(PRODUCT_SET_ID)
+            .build();
+    productSet = productSearchClient.createProductSet(productSetRequest);
+    productSetName = getName(productSet.getName());
+    formatProductSetName = ProductSetName.of(PROJECT_ID, COMPUTE_REGION, productSetName).toString();
+
+    /* add product to product set */
+    AddProductToProductSetRequest productToProductSetRequest =
+        AddProductToProductSetRequest.newBuilder()
+            .setName(formatProductSetName)
+            .setProduct(formatProductName)
+            .build();
+    productSearchClient.addProductToProductSet(productToProductSetRequest);
+
+    /* create reference image */
+    ReferenceImage createReferenceImage =
+        ReferenceImage.newBuilder().setUri(SAMPLE_BUCKET + "product_search/shoes_1.jpg").build();
+    CreateReferenceImageRequest imageRequest =
+        CreateReferenceImageRequest.newBuilder()
+            .setParent(formatProductName)
+            .setReferenceImage(createReferenceImage)
+            .build();
+    referenceImage = productSearchClient.createReferenceImage(imageRequest);
+    referenceImageName = getName(referenceImage.getName());
+    formatReferenceImageName =
+        ReferenceImageName.of(PROJECT_ID, COMPUTE_REGION, productName, referenceImageName)
+            .toString();
   }
 
   @AfterClass
   public static void tearDown() {
 
-    // delete product
-    DeleteProductRequest request =
+    /* delete reference image */
+    DeleteReferenceImageRequest imageRequest =
+        DeleteReferenceImageRequest.newBuilder().setName(formatReferenceImageName).build();
+    productSearchClient.deleteReferenceImage(imageRequest);
+
+    /* remove product from product set */
+    RemoveProductFromProductSetRequest removeProductRequest =
+        RemoveProductFromProductSetRequest.newBuilder()
+            .setName(formatProductSetName)
+            .setProduct(formatProductName)
+            .build();
+    productSearchClient.removeProductFromProductSet(removeProductRequest);
+
+    /* delete product set */
+    DeleteProductSetRequest deleteProductSetRequest =
+        DeleteProductSetRequest.newBuilder().setName(formatProductSetName).build();
+    productSearchClient.deleteProductSet(deleteProductSetRequest);
+
+    /* delete product */
+    DeleteProductRequest deleteProductRequest =
         DeleteProductRequest.newBuilder().setName(formatProductName).build();
-    productSearchClient.deleteProduct(request);
+    productSearchClient.deleteProduct(deleteProductRequest);
     productSearchClient.close();
 
     imageAnnotatorClient.close();
@@ -763,5 +842,74 @@ public class ITSystemTest {
     assertEquals(product.getName(), actualProduct.getName());
     assertEquals(PRODUCT_DISPLAY_NAME, actualProduct.getDisplayName());
     assertEquals(PRODUCT_CATEGORY, actualProduct.getProductCategory());
+  }
+
+  @Test
+  public void updateProductTest() {
+    Product updateProduct = product.toBuilder().addProductLabels(KEY_VALUE).build();
+    UpdateProductRequest request =
+        UpdateProductRequest.newBuilder().setProduct(updateProduct).build();
+    Product actualProduct = productSearchClient.updateProduct(request);
+    assertEquals(product.getName(), actualProduct.getName());
+    assertEquals(PRODUCT_DISPLAY_NAME, actualProduct.getDisplayName());
+    assertEquals(PRODUCT_CATEGORY, actualProduct.getProductCategory());
+    assertEquals(ImmutableList.of(KEY_VALUE), actualProduct.getProductLabelsList());
+    assertEquals(1, actualProduct.getProductLabelsCount());
+  }
+
+  @Test
+  public void listProductSetsTest() {
+    ListProductSetsRequest request =
+        ListProductSetsRequest.newBuilder().setParent(LOCATION_NAME).build();
+    for (ProductSet actualProductSet : productSearchClient.listProductSets(request).iterateAll()) {
+      if (productSet.getName().equals(actualProductSet.getName())) {
+        assertEquals(PRODUCT_SET_DISPLAY_NAME, actualProductSet.getDisplayName());
+      }
+    }
+  }
+
+  @Test
+  public void getProductSetTest() {
+    GetProductSetRequest request =
+        GetProductSetRequest.newBuilder().setName(formatProductSetName).build();
+    ProductSet actualProductSet = productSearchClient.getProductSet(request);
+    assertEquals(productSet.getName(), actualProductSet.getName());
+    assertEquals(PRODUCT_SET_DISPLAY_NAME, actualProductSet.getDisplayName());
+  }
+
+  @Test
+  public void listProductsInProductSetTest() {
+    ListProductsInProductSetRequest request =
+        ListProductsInProductSetRequest.newBuilder().setName(formatProductSetName).build();
+    for (Product actualProduct :
+        productSearchClient.listProductsInProductSet(request).iterateAll()) {
+      assertEquals(product.getName(), actualProduct.getName());
+      assertEquals(PRODUCT_DISPLAY_NAME, actualProduct.getDisplayName());
+      assertEquals(PRODUCT_CATEGORY, actualProduct.getProductCategory());
+    }
+  }
+
+  @Test
+  public void listReferenceImagesTest() {
+    ListReferenceImagesRequest request =
+        ListReferenceImagesRequest.newBuilder().setParent(formatProductName).build();
+    for (ReferenceImage actualReferenceImage :
+        productSearchClient.listReferenceImages(request).iterateAll()) {
+      assertEquals(referenceImage.getName(), actualReferenceImage.getName());
+      assertEquals(referenceImage.getUri(), actualReferenceImage.getUri());
+    }
+  }
+
+  @Test
+  public void getReferenceImageTest() {
+    GetReferenceImageRequest request =
+        GetReferenceImageRequest.newBuilder().setName(formatReferenceImageName).build();
+    ReferenceImage actualReferenceImage = productSearchClient.getReferenceImage(request);
+    assertEquals(referenceImage.getName(), actualReferenceImage.getName());
+    assertEquals(referenceImage.getUri(), actualReferenceImage.getUri());
+  }
+
+  private static String getName(String name) {
+    return name.substring(name.lastIndexOf("/")).replace("/", "");
   }
 }
