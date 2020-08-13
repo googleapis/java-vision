googleapis/java-video-intelligence

samples: updating to latest google-cloud-* dependencies (#723)

samples/snippets/src/main/java/com/example/video/Detect.java
@@ -17,8 +17,6 @@
 package com.example.video;
 
 import com.google.api.gax.grpc.OperationFuture;
-import com.google.cloud.videointelligence.spi.v1beta1.VideoIntelligenceServiceClient;
-import com.google.cloud.videointelligence.spi.v1beta1.VideoIntelligenceServiceSettings;
 import com.google.cloud.videointelligence.v1beta1.AnnotateVideoRequest;
 import com.google.cloud.videointelligence.v1beta1.AnnotateVideoResponse;
 import com.google.cloud.videointelligence.v1beta1.FaceAnnotation;
@@ -28,6 +26,8 @@ import com.google.cloud.videointelligence.v1beta1.LabelLocation;
 import com.google.cloud.videointelligence.v1beta1.Likelihood;
 import com.google.cloud.videointelligence.v1beta1.SafeSearchAnnotation;
 import com.google.cloud.videointelligence.v1beta1.VideoAnnotationResults;
+import com.google.cloud.videointelligence.v1beta1.VideoIntelligenceServiceClient;
+import com.google.cloud.videointelligence.v1beta1.VideoIntelligenceServiceSettings;
 import com.google.cloud.videointelligence.v1beta1.VideoSegment;
 import org.apache.commons.codec.binary.Base64;
 

samples/snippets/src/main/java/com/example/video/QuickstartSample.java
@@ -18,14 +18,14 @@ package com.example.video;
 
 // [START videointelligence_quickstart]
 import com.google.api.gax.grpc.OperationFuture;
-import com.google.cloud.videointelligence.spi.v1beta1.VideoIntelligenceServiceClient;
-import com.google.cloud.videointelligence.spi.v1beta1.VideoIntelligenceServiceSettings;
 import com.google.cloud.videointelligence.v1beta1.AnnotateVideoRequest;
 import com.google.cloud.videointelligence.v1beta1.AnnotateVideoResponse;
 import com.google.cloud.videointelligence.v1beta1.Feature;
 import com.google.cloud.videointelligence.v1beta1.LabelAnnotation;
 import com.google.cloud.videointelligence.v1beta1.LabelLocation;
 import com.google.cloud.videointelligence.v1beta1.VideoAnnotationResults;
+import com.google.cloud.videointelligence.v1beta1.VideoIntelligenceServiceClient;
+import com.google.cloud.videointelligence.v1beta1.VideoIntelligenceServiceSettings;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;

googleapis/java-vision

samples/snippets/src/main/java/com/example/vision/Detect.java
@@ -13,8 +13,6 @@
  */
 package com.example.vision;
 
-import com.google.cloud.vision.spi.v1.ImageAnnotatorClient;
-import com.google.cloud.vision.spi.v1.ImageAnnotatorSettings;
 import com.google.cloud.vision.v1.AnnotateImageRequest;
 import com.google.cloud.vision.v1.AnnotateImageResponse;
 import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
@@ -28,6 +26,8 @@ import com.google.cloud.vision.v1.FaceAnnotation;
 import com.google.cloud.vision.v1.Feature;
 import com.google.cloud.vision.v1.Feature.Type;
 import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.cloud.vision.v1.ImageAnnotatorSettings;
 import com.google.cloud.vision.v1.ImageSource;
 import com.google.cloud.vision.v1.LocationInfo;
 import com.google.cloud.vision.v1.Page;

samples/snippets/src/main/java/com/example/vision/QuickstartSample.java
@@ -18,7 +18,6 @@ package com.example.vision;
 
 // [START vision_quickstart]
 // Imports the Google Cloud client library
-import com.google.cloud.vision.spi.v1.ImageAnnotatorClient;
 import com.google.cloud.vision.v1.AnnotateImageRequest;
 import com.google.cloud.vision.v1.AnnotateImageResponse;
 import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
@@ -26,6 +25,7 @@ import com.google.cloud.vision.v1.EntityAnnotation;
 import com.google.cloud.vision.v1.Feature;
 import com.google.cloud.vision.v1.Feature.Type;
 import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
 import com.google.protobuf.ByteString;
 
 import java.nio.file.Files;