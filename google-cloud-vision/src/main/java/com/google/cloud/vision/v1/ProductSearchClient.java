f25b0ae2b6c1591f04169ace28d589b8ba259d7f
Jeff Ching <chingor@google.com>
2021-01-07 12:26:58 -0800

    restore deprecated resource name helpers

--- google-cloud-vision/src/main/java/com/google/cloud/vision/v1/ProductSearchClient.java
+++ google-cloud-vision/src/main/java/com/google/cloud/vision/v1/ProductSearchClient.java
@@ -28,6 +28,7 @@ import com.google.api.gax.paging.AbstractPagedListResponse;
 import com.google.api.gax.rpc.OperationCallable;
 import com.google.api.gax.rpc.PageContext;
 import com.google.api.gax.rpc.UnaryCallable;
+import com.google.api.pathtemplate.PathTemplate;
 import com.google.cloud.vision.v1.stub.ProductSearchStub;
 import com.google.cloud.vision.v1.stub.ProductSearchStubSettings;
 import com.google.common.util.concurrent.MoreExecutors;
@@ -113,6 +114,201 @@ public class ProductSearchClient implements BackgroundResource {
   private final ProductSearchSettings settings;
   private final ProductSearchStub stub;
   private final OperationsClient operationsClient;
+
+  private static final PathTemplate LOCATION_PATH_TEMPLATE =
+      PathTemplate.createWithoutUrlEncoding("projects/{project}/locations/{location}");
+
+  private static final PathTemplate PRODUCT_PATH_TEMPLATE =
+      PathTemplate.createWithoutUrlEncoding(
+          "projects/{project}/locations/{location}/products/{product}");
+
+  private static final PathTemplate PRODUCT_SET_PATH_TEMPLATE =
+      PathTemplate.createWithoutUrlEncoding(
+          "projects/{project}/locations/{location}/productSets/{product_set}");
+
+  private static final PathTemplate REFERENCE_IMAGE_PATH_TEMPLATE =
+      PathTemplate.createWithoutUrlEncoding(
+          "projects/{project}/locations/{location}/products/{product}/referenceImages/{reference_image}");
+
+  /**
+   * Formats a string containing the fully-qualified path to represent a location resource.
+   *
+   * @deprecated Use the {@link LocationName} class instead.
+   */
+  @Deprecated
+  public static final String formatLocationName(String project, String location) {
+    return LOCATION_PATH_TEMPLATE.instantiate(
+        "project", project,
+        "location", location);
+  }
+
+  /**
+   * Formats a string containing the fully-qualified path to represent a product resource.
+   *
+   * @deprecated Use the {@link ProductName} class instead.
+   */
+  @Deprecated
+  public static final String formatProductName(String project, String location, String product) {
+    return PRODUCT_PATH_TEMPLATE.instantiate(
+        "project", project,
+        "location", location,
+        "product", product);
+  }
+
+  /**
+   * Formats a string containing the fully-qualified path to represent a product_set resource.
+   *
+   * @deprecated Use the {@link ProductSetName} class instead.
+   */
+  @Deprecated
+  public static final String formatProductSetName(
+      String project, String location, String productSet) {
+    return PRODUCT_SET_PATH_TEMPLATE.instantiate(
+        "project", project,
+        "location", location,
+        "product_set", productSet);
+  }
+
+  /**
+   * Formats a string containing the fully-qualified path to represent a reference_image resource.
+   *
+   * @deprecated Use the {@link ReferenceImageName} class instead.
+   */
+  @Deprecated
+  public static final String formatReferenceImageName(
+      String project, String location, String product, String referenceImage) {
+    return REFERENCE_IMAGE_PATH_TEMPLATE.instantiate(
+        "project", project,
+        "location", location,
+        "product", product,
+        "reference_image", referenceImage);
+  }
+
+  /**
+   * Parses the project from the given fully-qualified path which represents a location resource.
+   *
+   * @deprecated Use the {@link LocationName} class instead.
+   */
+  @Deprecated
+  public static final String parseProjectFromLocationName(String locationName) {
+    return LOCATION_PATH_TEMPLATE.parse(locationName).get("project");
+  }
+
+  /**
+   * Parses the location from the given fully-qualified path which represents a location resource.
+   *
+   * @deprecated Use the {@link LocationName} class instead.
+   */
+  @Deprecated
+  public static final String parseLocationFromLocationName(String locationName) {
+    return LOCATION_PATH_TEMPLATE.parse(locationName).get("location");
+  }
+
+  /**
+   * Parses the project from the given fully-qualified path which represents a product resource.
+   *
+   * @deprecated Use the {@link ProductName} class instead.
+   */
+  @Deprecated
+  public static final String parseProjectFromProductName(String productName) {
+    return PRODUCT_PATH_TEMPLATE.parse(productName).get("project");
+  }
+
+  /**
+   * Parses the location from the given fully-qualified path which represents a product resource.
+   *
+   * @deprecated Use the {@link ProductName} class instead.
+   */
+  @Deprecated
+  public static final String parseLocationFromProductName(String productName) {
+    return PRODUCT_PATH_TEMPLATE.parse(productName).get("location");
+  }
+
+  /**
+   * Parses the product from the given fully-qualified path which represents a product resource.
+   *
+   * @deprecated Use the {@link ProductName} class instead.
+   */
+  @Deprecated
+  public static final String parseProductFromProductName(String productName) {
+    return PRODUCT_PATH_TEMPLATE.parse(productName).get("product");
+  }
+
+  /**
+   * Parses the project from the given fully-qualified path which represents a product_set resource.
+   *
+   * @deprecated Use the {@link ProductSetName} class instead.
+   */
+  @Deprecated
+  public static final String parseProjectFromProductSetName(String productSetName) {
+    return PRODUCT_SET_PATH_TEMPLATE.parse(productSetName).get("project");
+  }
+
+  /**
+   * Parses the location from the given fully-qualified path which represents a product_set
+   * resource.
+   *
+   * @deprecated Use the {@link ProductSetName} class instead.
+   */
+  @Deprecated
+  public static final String parseLocationFromProductSetName(String productSetName) {
+    return PRODUCT_SET_PATH_TEMPLATE.parse(productSetName).get("location");
+  }
+
+  /**
+   * Parses the product_set from the given fully-qualified path which represents a product_set
+   * resource.
+   *
+   * @deprecated Use the {@link ProductSetName} class instead.
+   */
+  @Deprecated
+  public static final String parseProductSetFromProductSetName(String productSetName) {
+    return PRODUCT_SET_PATH_TEMPLATE.parse(productSetName).get("product_set");
+  }
+
+  /**
+   * Parses the project from the given fully-qualified path which represents a reference_image
+   * resource.
+   *
+   * @deprecated Use the {@link ReferenceImageName} class instead.
+   */
+  @Deprecated
+  public static final String parseProjectFromReferenceImageName(String referenceImageName) {
+    return REFERENCE_IMAGE_PATH_TEMPLATE.parse(referenceImageName).get("project");
+  }
+
+  /**
+   * Parses the location from the given fully-qualified path which represents a reference_image
+   * resource.
+   *
+   * @deprecated Use the {@link ReferenceImageName} class instead.
+   */
+  @Deprecated
+  public static final String parseLocationFromReferenceImageName(String referenceImageName) {
+    return REFERENCE_IMAGE_PATH_TEMPLATE.parse(referenceImageName).get("location");
+  }
+
+  /**
+   * Parses the product from the given fully-qualified path which represents a reference_image
+   * resource.
+   *
+   * @deprecated Use the {@link ReferenceImageName} class instead.
+   */
+  @Deprecated
+  public static final String parseProductFromReferenceImageName(String referenceImageName) {
+    return REFERENCE_IMAGE_PATH_TEMPLATE.parse(referenceImageName).get("product");
+  }
+
+  /**
+   * Parses the reference_image from the given fully-qualified path which represents a
+   * reference_image resource.
+   *
+   * @deprecated Use the {@link ReferenceImageName} class instead.
+   */
+  @Deprecated
+  public static final String parseReferenceImageFromReferenceImageName(String referenceImageName) {
+    return REFERENCE_IMAGE_PATH_TEMPLATE.parse(referenceImageName).get("reference_image");
+  }
 
   /** Constructs an instance of ProductSearchClient with default settings. */
   public static final ProductSearchClient create() throws IOException {
