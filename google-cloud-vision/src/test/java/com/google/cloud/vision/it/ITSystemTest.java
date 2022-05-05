f76d58b8bac49d3b3799aa3d53621c1ad9031097
Neenu1995 <neenushaji@google.com>
2022-05-05 10:59:02 -0400

    chore: fix detectTextTest

--- google-cloud-vision/src/test/java/com/google/cloud/vision/it/ITSystemTest.java
+++ google-cloud-vision/src/test/java/com/google/cloud/vision/it/ITSystemTest.java
@@ -393,7 +393,7 @@ public class ITSystemTest {
         actual.add(annotation.getDescription());
       }
     }
-    assertThat(actual).contains("37%");
+    assertThat(actual).contains("PS4");
   }
 
   @Test
